514f158dbb5981a0fc4d4f093db2102594310aae
SonuTheCode <sy2440052@gmail.com>
2025-12-21 12:33:32 +0530

    Implement Bank class with user account display

--- OOPS/simple.py
+++ OOPS/simple.py
@@ -1,1 +1,21 @@
+class Bank:
+    
+    def __init__(self,u,b):
+        self.username= u
+        self.balance= b
+        
+    def show(self):
+        print(f"Account Holder :- {self.username} | balance :- {self.balance}")
+     
+     
+user1 = Bank("Sonu Yadav",5000)
+user1.show()
 
+user2 = Bank("Rikesh Singh",4000)
+user2.show()
+
+
+OUTPUT:----
+
+Account Holder :- Sonu Yadav | balance :- 5000
+Account Holder :- Rikesh Singh | balance :- 4000
