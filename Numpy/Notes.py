3a47c4ddcba361607bbb228400b6ae14ba5556b1
SonuTheCode <sy2440052@gmail.com>
2025-12-25 09:16:27 +0530

    Enhance Notes.py with NumPy details
    Added information about NumPy, its purpose, and advantages.

---
+++ Numpy/Notes.py
@@ -0,0 +1,14 @@
+🔢 NumPy in Python :---
+
+NumPy ka full form hai Numerical Python.
+Ye Python ka powerful library hai jo numbers, arrays, matrices, math & scientific calculation ke liye use hota hai.
+
+  
+  
+🔹 NumPy kyon use karte hain?
+
+• Fast calculation (C language se bhi fast)
+• Large data handle kar sakta hai
+• Machine Learning, Data Science, AI mein use hota hai
+• List se zyada efficient array
+
