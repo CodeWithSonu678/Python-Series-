80b1fff5e3ff264322313c27d7f564e19c17e950
SonuTheCode <sy2440052@gmail.com>
2025-12-27 14:28:59 +0530

    Create Notes.py

---
+++ MatPlotLib/Notes.py
@@ -0,0 +1,41 @@
+Matplotlib in Python 📊
+
+(Matplotlib = Python ki plotting / graph library)
+
+🔹 Matplotlib kya hai?
+
+Matplotlib Python ki library hai jisse hum:
+•Graph
+•Chart
+•Plot
+•Visualization
+banate hain (data ko visually samajhne ke liye).
+
+👉 Data Analysis + Machine Learning + Projects me bahut use hoti hai.
+
+🔹 Important Functions (Exam + Interview)
+
+Function      Use
+
+plt.plot()    Line graph(📌 Use: Trend dekhne ke liye (time vs value))
+
+plt.bar().    Bar chart(📌 Use: Comparison ke liye)
+
+plt.scatter() Scatter plot(📌 Use: Relationship / pattern dekhne ke liye )
+
+plt.pie() Pie chart(📌 Use: Percentage distribution)
+
+plt.xlabel()  X label
+plt.ylabel()  Y label
+plt.title()   Title
+plt.legend()  Legend
+plt.grid()    Grid (background ka line show karta h )
+plt.show()    Graph display
+
+🔹 Matplotlib ML / Industry me kaise use hota hai?
+
+•Data visualization
+•Model accuracy graph
+•Loss vs Epoch graph
+•Sales / Report dashboard
+•Trend analysis
